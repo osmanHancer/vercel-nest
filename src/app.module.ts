c32234d8ec561e4e7cb743ac934a5bf18c704370
Osman Hançer <ohancer74@gmail.com>
2024-09-22 19:00:26 +0300

    ff

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { DavalarModule } from './davalar/davalar.module';
 		"username": "avnadmin",
 		"password": "AVNS_UvV-Ign5rZP5i099-PL",
 		"database": "test",
-		  timezone: "Z",
+		  "timezone":"UTC+3",
 		"synchronize": true,
 		ssl: {
 			rejectUnauthorized: true,
